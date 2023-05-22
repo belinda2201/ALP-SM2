2f405163c92277c0d648872d9680069832079b58
Belinda <121992274+belinda2201@users.noreply.github.com>
2023-05-22 23:24:14 +0700

    ALP AD

---
+++ ALP APP DEV/ALP APP DEV/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ALP_APP_DEV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_signIn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_signUp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(632, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 527);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(835, 546);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(835, 686);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(693, 608);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(411, 38);
+            this.txt_username.TabIndex = 3;
+            this.txt_username.Enter += new System.EventHandler(this.txt_username_Enter);
+            this.txt_username.Leave += new System.EventHandler(this.txt_username_Leave);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(693, 759);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(411, 38);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.Tag = "";
+            this.txt_password.Enter += new System.EventHandler(this.txt_password_Enter);
+            this.txt_password.Leave += new System.EventHandler(this.txt_password_Leave);
+            // 
+            // btn_signIn
+            // 
+            this.btn_signIn.Location = new System.Drawing.Point(763, 847);
+            this.btn_signIn.Name = "btn_signIn";
+            this.btn_signIn.Size = new System.Drawing.Size(282, 71);
+            this.btn_signIn.TabIndex = 5;
+            this.btn_signIn.Text = "Sign In";
+            this.btn_signIn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(662, 974);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Don\'t have an account? ";
+            // 
+            // label_signUp
+            // 
+            this.label_signUp.AutoSize = true;
+            this.label_signUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_signUp.ForeColor = System.Drawing.Color.White;
+            this.label_signUp.Location = new System.Drawing.Point(989, 974);
+            this.label_signUp.Name = "label_signUp";
+            this.label_signUp.Size = new System.Drawing.Size(122, 32);
+            this.label_signUp.TabIndex = 7;
+            this.label_signUp.Text = "Sign Up";
+            this.label_signUp.Click += new System.EventHandler(this.label_signUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1819, 1369);
+            this.Controls.Add(this.label_signUp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_signIn);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_signIn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_signUp;
+    }
+}
+
---
+++ ALP APP DEV/ALP APP DEV/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ALP_APP_DEV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection sqlConnection;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlDataAdapter;
+        MySqlDataReader sqlDataReader;
+        string connection = "server=localhost;user=root;pwd=0318053478Bel.;database=db_concert";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_username.Text = "john@mail.com";
+            txt_username.ForeColor = SystemColors.GrayText;
+            txt_password.Text = "123456";
+            txt_password.ForeColor = SystemColors.GrayText;
+
+            this.ActiveControl = null;
+            pictureBox1.Focus(); //shit isnt working the heck?????
+        }
+
+        /*biar di txt_username ada tulisannya buat panduan
+        kalo di click tulisannya hilang*/
+        private void txt_username_Enter(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "john@mail.com")
+            {
+                txt_username.Text = "";
+                txt_username.ForeColor = SystemColors.WindowText;
+            }
+        }
+        private void txt_password_Enter(object sender, EventArgs e)
+        {
+            if (txt_password.Text == "123456")
+            {
+                txt_password.Text = "";
+                txt_password.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        //literally kebalikkannya yg atas
+        private void txt_username_Leave(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "")
+            {
+                txt_username.Text = "john@mail.com";
+                txt_username.ForeColor = SystemColors.GrayText;
+            }
+        }
+        private void txt_password_Leave(object sender, EventArgs e)
+        {
+            if (txt_password.Text == "")
+            {
+                txt_password.Text = "123456";
+                txt_password.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void label_signUp_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
